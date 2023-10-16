971cda4a5633cc198cf211b516ef75412cdb2013
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-10-16 13:55:43 +0100

    feat: setup routes for signin, signup, signout and currentUser, installed express-validator

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { json } from "body-parser";
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/siginin";
+import { siginOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
 
 const app = express();
 app.use(json());
 
-app.get("/api/users/currentUser", (req, res) => {
-  res.send("hi there!");
-});
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(siginOutRouter);
+app.use(signUpRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!");
