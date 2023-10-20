f796732e5d5aa1160a2dca0e91b54dde503e7ad2
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-10-20 23:26:37 +0100

    feat: installed cookie-session, and setup cookie session to only accept jwt over an http connection

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,9 +8,17 @@ import { siginOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
 import { errorHandler } from "./routes/middlewares/errors-handler";
 import { NotFoundError } from "./errors/not-found-errors";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
