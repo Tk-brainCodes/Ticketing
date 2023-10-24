348c15a194735cb00993fdc3c5df24548a6fc4fa
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-10-24 23:42:37 +0100

    feat: added form validation to signin route

--- auth/src/routes/siginin.ts
+++ auth/src/routes/siginin.ts
@@ -1,9 +1,22 @@
-import express from "express";
-
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "./../errors/request-validation-errors";
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("signin");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Enter a valid email"),
+    body("password").trim().notEmpty().withMessage("Password is required"),
+  ],
+  (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export { router as signInRouter };
