3a27c282fc9180ba24bdef461cdbf8d9fb28bcb6
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-10-26 07:42:28 +0100

    feat: completed the signin route, added password hash checking and validation

--- auth/src/routes/siginin.ts
+++ auth/src/routes/siginin.ts
@@ -1,21 +1,61 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "./../errors/request-validation-errors";
+import { body } from "express-validator";
+import { validateRequest } from "./middlewares/valid-request";
+import { User } from "../models/user";
+import { BadRequest } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
+
 const router = express.Router();
 
 router.post(
   "/api/users/signin",
   [
     body("email").isEmail().withMessage("Enter a valid email"),
-    body("password").trim().notEmpty().withMessage("Password is required"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
   ],
-  (req: Request, res: Response) => {
+  validateRequest,
+  async (req: Request, res: Response) => {
     const { email, password } = req.body;
 
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
+    const existingUser = User.findOne({ email });
+
+    //check if user exists in mongo db
+    if (!existingUser) {
+      throw new BadRequest("Invalid credentials");
     }
+
+    //hash the password and check against the password stored in mongo db
+    const passwordMatch = await Password.compare(
+      //@ts-ignore
+      existingUser.password,
+      password
+    );
+
+    if (!passwordMatch) {
+      throw new BadRequest("Invalid Credentials");
+    }
+
+    //Generate JWT
+    const userJwt = jwt.sign(
+      {
+        //@ts-ignore
+        id: existingUser.id,
+        //@ts-ignore
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    //store jwt on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
   }
 );
 
--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -3,7 +3,7 @@ import { promisify } from "util";
 
 const scryptAsync = promisify(scrypt);
 
-export class Passowrd {
+export class Password {
   static async toHash(password: string) {
     const salt = randomBytes(8).toString("hex");
     const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
@@ -11,7 +11,7 @@ export class Passowrd {
     return `${buffer.toString("hex")}.${salt}`;
   }
 
-  static async comapre(storedPassword: string, suppliedPassword: string) {
+  static async compare(storedPassword: string, suppliedPassword: string) {
     const [hashedPassword, salt] = storedPassword.split(".");
     const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
 
